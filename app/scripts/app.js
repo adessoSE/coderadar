622c808cc93219db574de3ef894c4f68a48640b6
Tom Hombergs <tom.hombergs@gmail.com>
2015-09-20 23:33:14 +0200

    initial setup

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name coderadarApp
+ * @description
+ * # coderadarApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('coderadarApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coderadarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coderadarApp
+ */
+angular.module('coderadarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
