88703c6996bb74a4708c71ef1156de71dee55d9f
Tom Hombergs <tom.hombergs@gmail.com>
2016-04-24 21:22:48 +0200

    removed unused method

--- plugins/findbugs-adapter-plugin/src/main/java/org/wickedsource/coderadar/analyzer/findbugs/xsd/BugCollectionParser.java
+++ plugins/findbugs-adapter-plugin/src/main/java/org/wickedsource/coderadar/analyzer/findbugs/xsd/BugCollectionParser.java
@@ -1,13 +1,9 @@
 package org.wickedsource.coderadar.analyzer.findbugs.xsd;
 
-import org.wickedsource.coderadar.analyzer.api.AnalyzerException;
-import org.xml.sax.SAXParseException;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.ByteArrayInputStream;
-import java.util.List;
 
 public class BugCollectionParser {
 
@@ -24,11 +20,4 @@ public class BugCollectionParser {
         return (BugCollection) parser.unmarshal(new ByteArrayInputStream(bugCollection));
     }
 
-    public void validate(byte[] bugCollection) throws AnalyzerException {
-        FindbugsReportXsdValidator validator = new FindbugsReportXsdValidator();
-        List<SAXParseException> validationErrors = validator.validate(bugCollection, FindbugsReportXsdValidator.ValidationLevel.FATAL);
-        if (!validationErrors.isEmpty()) {
-            throw new AnalyzerException("Not a valid Findbugs report! First XSD validation error:", validationErrors.get(0));
-        }
-    }
 }
