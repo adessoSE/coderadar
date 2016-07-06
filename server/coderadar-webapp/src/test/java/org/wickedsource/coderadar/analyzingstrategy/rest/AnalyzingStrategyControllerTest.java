6e050005e6828e57f5ad47c9b0b7e5eeeb22869c
Tom Hombergs <tom.hombergs@gmail.com>
2016-07-06 21:06:09 +0200

    docs

--- server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/analyzingstrategy/rest/AnalyzingStrategyControllerTest.java
+++ server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/analyzingstrategy/rest/AnalyzingStrategyControllerTest.java
@@ -43,7 +43,7 @@ public class AnalyzingStrategyControllerTest extends ControllerTestTemplate {
                                 linkWithRel("project").description("Link to the project resource this AnalyzingStrategy belongs to.")),
                         requestFields(
                                 fields.withPath("active").description("Defines if this analyzing strategy is active. If it is not active (or there is no strategy for the project at all), no commits of the project will be analyzed. Analyses of commits that are already queued will be finished, however."),
-                                fields.withPath("rescan").description("Set this to true when updating an AnalyzingStrategy if you want to delete all existing analysis results and restart analysis with the new strategy. You will get an error response if you try rescanning a project while there are running or queued analysis jobs (i.e. when the last scan over all commits is not yet finished)."),
+                                fields.withPath("rescan").description("Set this to true when updating an AnalyzingStrategy if you want to delete all existing analysis results and restart analysis with the new strategy. You will get an error response if you try rescanning a project while there are running or queued analysis jobs (i.e. when the previous scan over all commits is not yet finished)."),
                                 fields.withPath("fromDate").description("Date (in milliseconds since epoch) from which to start analyzing commits. Commits before this date are ignored during analysis. If no date is specified, all commits will be analyzed."))));
 
     }
