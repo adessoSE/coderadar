1684f7873f69357a99945bf717505d3d441a7d5e
Tom Hombergs <tom.hombergs@gmail.com>
2016-07-20 21:26:32 +0200

    replaced log4j with logback for tests

--- server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/commit/rest/CommitControllerTest.java
+++ server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/commit/rest/CommitControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.hateoas.PagedResources;
 import org.wickedsource.coderadar.testframework.category.ControllerTest;
 import org.wickedsource.coderadar.testframework.template.ControllerTestTemplate;
 
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.wickedsource.coderadar.factories.databases.DbUnitFactory.Commits.SINGLE_PROJECT_WITH_COMMITS;
@@ -21,7 +22,13 @@ public class CommitControllerTest extends ControllerTestTemplate {
     public void getCommits() throws Exception {
         mvc().perform(get("/projects/1/commits"))
                 .andExpect(status().isOk())
-                .andExpect(contains(PagedResources.class));
+                .andExpect(contains(PagedResources.class))
+                .andDo(document("commit/list",
+                        links(halLinks(),
+                                linkWithRel("self").description("Link to the current page of commits."),
+                                linkWithRel("next").description("Link to the next page of commits."),
+                                linkWithRel("last").description("Link to the last page of commits."),
+                                linkWithRel("first").description("Link to the first page of commits."))));
     }
 
 }
