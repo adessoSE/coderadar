0033457a525e8c13d191c1f8b2489d6f92b92e43
Tom Hombergs <tom.hombergs@gmail.com>
2016-07-25 00:24:23 +0200

    minor documentation fixes

--- server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/commit/rest/CommitControllerTest.java
+++ server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/commit/rest/CommitControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.hateoas.PagedResources;
 import org.wickedsource.coderadar.testframework.category.ControllerTest;
 import org.wickedsource.coderadar.testframework.template.ControllerTestTemplate;
 
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.wickedsource.coderadar.factories.databases.DbUnitFactory.Commits.SINGLE_PROJECT_WITH_COMMITS;
@@ -21,7 +21,7 @@ public class CommitControllerTest extends ControllerTestTemplate {
     @ExpectedDatabase(SINGLE_PROJECT_WITH_COMMITS)
     public void getCommits() throws Exception {
         ConstrainedFields fields = fields(CommitResource.class);
-        mvc().perform(get("/projects/1/commits"))
+        mvc().perform(get("/projects/1/commits?page=3&size=5"))
                 .andExpect(status().isOk())
                 .andExpect(contains(PagedResources.class))
                 .andDo(document("commit/list",
