94e4d2c44399c270d2bd5c9dfeb44a53149327bc
Tom Hombergs <tom.hombergs@gmail.com>
2016-10-16 22:21:24 +0200

    added security configuration that only secures the "/actuator" endpoint so far (fixes #75)

---
+++ server/coderadar-webapp/src/main/java/org/wickedsource/coderadar/security/CoderadarSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package org.wickedsource.coderadar.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class CoderadarSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator").hasAnyRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+}
