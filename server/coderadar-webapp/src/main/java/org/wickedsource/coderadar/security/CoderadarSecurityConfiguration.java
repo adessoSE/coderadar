dbb6a20ab59f0aab79bc7c75bd9aea539c3931e8
Tom Hombergs <tom.hombergs@gmail.com>
2016-10-18 21:20:57 +0200

    deactivated Spring Security's CSRF protection (fixes #83)

--- server/coderadar-webapp/src/main/java/org/wickedsource/coderadar/security/CoderadarSecurityConfiguration.java
+++ server/coderadar-webapp/src/main/java/org/wickedsource/coderadar/security/CoderadarSecurityConfiguration.java
@@ -18,6 +18,8 @@ public class CoderadarSecurityConfiguration extends WebSecurityConfigurerAdapter
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
+
+        http.csrf().disable();
     }
 
     @Autowired
