8cd23ee1ae855e10ffd95bef33eebccfa65f47f2
Tom Hombergs <tom.hombergs@gmail.com>
2016-12-11 19:59:17 +0100

    generified MetricsValueTreeResource to support different payloads

--- server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/testframework/template/ResultMatchers.java
+++ server/coderadar-webapp/src/test/java/org/wickedsource/coderadar/testframework/template/ResultMatchers.java
@@ -1,5 +1,6 @@
 package org.wickedsource.coderadar.testframework.template;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.hateoas.PagedResources;
@@ -37,6 +38,25 @@ public class ResultMatchers {
                 assertThat(object).isNotNull();
             } catch (Exception e) {
                 throw new RuntimeException(String.format("expected JSON representation of class %s but found '%s'", clazz, json), e);
+            }
+        };
+    }
+
+    /**
+     * Tests if the response contains the JSON representation of an object of the given type.
+     *
+     * @param typeReference the type of the expected object.
+     * @param <T>           the type of the expected object.
+     * @return ResultMatcher that performs the test described above.
+     */
+    public static <T> ResultMatcher containsResource(TypeReference<T> typeReference) {
+        return result -> {
+            String json = result.getResponse().getContentAsString();
+            try {
+                T object = fromJson(json, typeReference);
+                assertThat(object).isNotNull();
+            } catch (Exception e) {
+                throw new RuntimeException(String.format("expected JSON representation of class %s but found '%s'", typeReference, json), e);
             }
         };
     }
