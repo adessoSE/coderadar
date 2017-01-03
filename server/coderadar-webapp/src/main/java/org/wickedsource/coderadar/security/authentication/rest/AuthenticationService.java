4081271e2c2a73312846a6666f28083b293b4ac4
diplinf <andi0000>
2017-01-03 08:16:09 +0100

    token validity increased up to 7 days

--- server/coderadar-webapp/src/main/java/org/wickedsource/coderadar/security/authentication/rest/AuthenticationService.java
+++ server/coderadar-webapp/src/main/java/org/wickedsource/coderadar/security/authentication/rest/AuthenticationService.java
@@ -11,23 +11,18 @@ import com.auth0.jwt.algorithms.Algorithm;
 @Service
 public class AuthenticationService {
 
-    private static final Integer TOKEN_VALIDITY_DURATION_IN_MINUTES = 60;
+    private static final Integer TOKEN_VALIDITY_DURATION_IN_DAYS = 7;
 
     public String createToken(UserLoginResource userLoginResource) {
         DateTime dateTime = new DateTime();
-        dateTime.plusMinutes(TOKEN_VALIDITY_DURATION_IN_MINUTES);
-        byte[] secretKey = generateSecretKey();
-        String jwtId = putSecretKey(secretKey);
+        dateTime.plusDays(TOKEN_VALIDITY_DURATION_IN_DAYS);
+        byte[] secretKey = getSecretKey();
+        String jwtId = UUID.randomUUID().toString();
         return JWT.create().withIssuer(userLoginResource.getUsername()).withExpiresAt(dateTime.toDate()).withJWTId(jwtId).sign(Algorithm.HMAC256(secretKey));
     }
 
-    private String putSecretKey(byte[] secretKey) {
-        // TODO save secret key for verification in a distributed cache or in the database with jwtId as key and secretKey as values
-        return UUID.randomUUID().toString();
-    }
-
-    private byte[] generateSecretKey() {
-        // TODO generate secret bytes
+    private byte[] getSecretKey() {
+        // TODO get secret bytes from key provider
         return new byte[32];
     }
 }
