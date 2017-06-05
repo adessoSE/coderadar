e9af1f7efc1956ef6c749fba4f1026ef1a2777f4
kuksin <arthur.kuksin@gmail.com>
2017-06-05 12:51:00 +0200

    Change security configuration, add api documentation

--- coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/security/CoderadarSecurityConfiguration.java
+++ coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/security/CoderadarSecurityConfiguration.java
@@ -2,8 +2,6 @@ package org.wickedsource.coderadar.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.FixedPrincipalExtractor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
