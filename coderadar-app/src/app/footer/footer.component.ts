6d97e2a287acf513d5292251fc9c8bd67e660e56
Maxim Atanasov <atanasovmaksim1@gmail.com>
2019-03-11 21:49:59 +0100

    removed more inline css

--- coderadar-app/src/app/footer/footer.component.ts
+++ coderadar-app/src/app/footer/footer.component.ts
@@ -5,11 +5,5 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FooterComponent {
 }
