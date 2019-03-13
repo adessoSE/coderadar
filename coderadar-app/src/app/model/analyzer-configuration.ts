35fccb1e0ca3a928235c23b1eebe73dbcb707839
Maxim Atanasov <atanasovmaksim1@gmail.com>
2019-03-13 17:23:22 +0100

    analyzerConfigartion constructor fix

--- coderadar-app/src/app/model/analyzer-configuration.ts
+++ coderadar-app/src/app/model/analyzer-configuration.ts
@@ -3,7 +3,7 @@ export class AnalyzerConfiguration {
   analyzerName: string;
   enabled: boolean;
 
-  constructor(name: string, enabled: boolean) {
+  constructor(name?: string, enabled?: boolean) {
     this.analyzerName = name;
     this.enabled = enabled;
   }
