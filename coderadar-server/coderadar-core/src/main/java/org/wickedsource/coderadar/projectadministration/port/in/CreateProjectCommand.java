1c2a6b163ba45dc520a95b1b050c37b779fc4259
Sebastian Trzpiot <mail@trzpiot.de>
2019-03-20 09:50:53 +0100

    Add variables to CreateProjectCommand

--- coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/projectadministration/port/in/CreateProjectCommand.java
+++ coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/projectadministration/port/in/CreateProjectCommand.java
@@ -6,5 +6,13 @@ import lombok.Value;
 
 @Value
 public class CreateProjectCommand {
-
+    Long id;
+    String name;
+    String workdir;
+    String vcsUsername;
+    String vcsPassword;
+    URL vcsUrl;
+    Boolean vcsOnline;
+    Date start;
+    Date end;
 }
