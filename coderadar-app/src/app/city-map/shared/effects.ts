bf8cc0e27a534467e5246ad39da70859fc15bd19
Maxim Atanasov <atanasovmaksim1@gmail.com>
2019-03-21 22:52:27 +0100

    removed unneeded log statements

--- coderadar-app/src/app/city-map/shared/effects.ts
+++ coderadar-app/src/app/city-map/shared/effects.ts
@@ -3,21 +3,13 @@ import {Actions, Effect, ofType} from '@ngrx/effects';
 import {of} from 'rxjs';
 import * as actions from './actions';
 import {CommitService} from '../../service/commit.service';
-import {ICommitsGetResponse} from '../interfaces/ICommitsGetResponse';
 import {ICommitsGetErrorResponse} from '../interfaces/ICommitsGetErrorResponse';
 import {IDeltaTreeGetErrorResponse} from '../interfaces/IDeltaTreeGetErrorResponse';
 import {IDeltaTreeGetResponse} from '../interfaces/IDeltaTreeGetResponse';
 import {MetricService} from '../../service/metric.service';
 import {catchError, map, switchMap, mergeMap} from 'rxjs/operators';
 import { IActionWithPayload } from '../interfaces/IActionWithPayload';
-import { IAvailableMetricsGetResponse } from '../interfaces/IAvailableMetricsGetResponse';
-import { IAvailableMetricsGetErrorResponse } from '../interfaces/IAvailableMetricsGetErrorResponse';
-import { IMetric } from '../interfaces/IMetric';
-import { AppConfig } from '../../AppConfig';
 import {LOAD_AVAILABLE_METRICS, LOAD_COMMITS, LOAD_METRIC_TREE} from './actions';
-import {INode} from '../interfaces/INode';
-import {ICommit} from '../interfaces/ICommit';
-import {IMetricMapping} from '../interfaces/IMetricMapping';
 
 @Injectable()
 export class AppEffects {
@@ -30,7 +22,6 @@ export class AppEffects {
                 () => this.commitService.loadCommits(this.currentProjectId)
                     .pipe(
                         map((result: any) => {
-                          console.log(result);
                           return actions.loadCommitsSuccess(result);
                         }),
                         catchError((response: ICommitsGetErrorResponse) => {
@@ -86,7 +77,6 @@ export class AppEffects {
                 ];
             }),
             catchError((response: IDeltaTreeGetErrorResponse) => {
-                console.log(response);
                 return of(actions.loadMetricTreeError(response.error));
             })
         )
