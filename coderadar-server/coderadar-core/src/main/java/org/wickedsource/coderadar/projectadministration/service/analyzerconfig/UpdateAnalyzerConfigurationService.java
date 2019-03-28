7f0a23d8c8248bad0df6384fdd796bce6a2db025
Sebastian Trzpiot <mail@trzpiot.de>
2019-03-28 20:44:09 +0100

    Modify UpdateAnalyzerConfigurationService
    The configuration to be changed should first be loaded and then
    modified.

--- coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/projectadministration/service/analyzerconfig/UpdateAnalyzerConfigurationService.java
+++ coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/projectadministration/service/analyzerconfig/UpdateAnalyzerConfigurationService.java
@@ -3,6 +3,7 @@ package org.wickedsource.coderadar.projectadministration.service.analyzerconfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.wickedsource.coderadar.projectadministration.domain.AnalyzerConfiguration;
+import org.wickedsource.coderadar.projectadministration.port.driven.analyzerconfig.GetAnalyzerConfigurationPort;
 import org.wickedsource.coderadar.projectadministration.port.driven.analyzerconfig.UpdateAnalyzerConfigurationPort;
 import org.wickedsource.coderadar.projectadministration.port.driver.analyzerconfig.UpdateAnalyzerConfigurationCommand;
 import org.wickedsource.coderadar.projectadministration.port.driver.analyzerconfig.UpdateAnalyzerConfigurationUseCase;
@@ -10,20 +11,22 @@ import org.wickedsource.coderadar.projectadministration.port.driver.analyzerconf
 @Service
 public class UpdateAnalyzerConfigurationService implements UpdateAnalyzerConfigurationUseCase {
 
-  private final UpdateAnalyzerConfigurationPort port;
+  private final GetAnalyzerConfigurationPort getAnalyzerConfigurationPort;
+  private final UpdateAnalyzerConfigurationPort updateAnalyzerConfigurationPort;
 
   @Autowired
-  public UpdateAnalyzerConfigurationService(UpdateAnalyzerConfigurationPort port) {
-    this.port = port;
+  public UpdateAnalyzerConfigurationService(UpdateAnalyzerConfigurationPort updateAnalyzerConfigurationPort, GetAnalyzerConfigurationPort getAnalyzerConfigurationPort) {
+    this.updateAnalyzerConfigurationPort = updateAnalyzerConfigurationPort;
+    this.getAnalyzerConfigurationPort = getAnalyzerConfigurationPort;
   }
 
   @Override
   public void update(
       UpdateAnalyzerConfigurationCommand command) {
-    AnalyzerConfiguration analyzerConfiguration = new AnalyzerConfiguration();
-    analyzerConfiguration.setId(command.getId());
+    AnalyzerConfiguration analyzerConfiguration =
+            getAnalyzerConfigurationPort.getAnalyzerConfiguration(command.getId());
     analyzerConfiguration.setAnalyzerName(command.getAnalyzerName());
     analyzerConfiguration.setEnabled(command.getEnabled());
-    port.update(analyzerConfiguration);
+    updateAnalyzerConfigurationPort.update(analyzerConfiguration);
   }
 }
