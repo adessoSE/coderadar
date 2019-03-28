e5378141ae86856cd6828a56af64f024ab78232b
Sebastian Trzpiot <mail@trzpiot.de>
2019-03-28 21:31:06 +0100

    Load module before modifying
    The module to be changed should first be loaded and then modified.

--- coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/projectadministration/service/module/UpdateModuleService.java
+++ coderadar-server/coderadar-core/src/main/java/org/wickedsource/coderadar/projectadministration/service/module/UpdateModuleService.java
@@ -3,6 +3,7 @@ package org.wickedsource.coderadar.projectadministration.service.module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.wickedsource.coderadar.projectadministration.domain.Module;
+import org.wickedsource.coderadar.projectadministration.port.driven.module.GetModulePort;
 import org.wickedsource.coderadar.projectadministration.port.driven.module.UpdateModulePort;
 import org.wickedsource.coderadar.projectadministration.port.driver.module.UpdateModuleCommand;
 import org.wickedsource.coderadar.projectadministration.port.driver.module.UpdateModuleUseCase;
@@ -10,16 +11,18 @@ import org.wickedsource.coderadar.projectadministration.port.driver.module.Updat
 @Service
 public class UpdateModuleService implements UpdateModuleUseCase {
 
+  private final GetModulePort getModulePort;
   private final UpdateModulePort updateModulePort;
 
   @Autowired
-  public UpdateModuleService(UpdateModulePort updateModulePort) {
+  public UpdateModuleService(GetModulePort getModulePort, UpdateModulePort updateModulePort) {
+    this.getModulePort = getModulePort;
     this.updateModulePort = updateModulePort;
   }
 
   @Override
   public Module updateModule(UpdateModuleCommand command) {
-    Module module = new Module();
+    Module module = getModulePort.get(command.getId());
     module.setPath(command.getPath());
     module = updateModulePort.updateModule(module);
     return module;
