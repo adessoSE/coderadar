45a645bc742c12f8e2e335f7ab275165c6e43405
Sebastian Gabriel Trzpiot <mail@trzpiot.de>
2019-04-18 11:15:54 +0200

    Convert the entity Module to Neo4j

--- coderadar-core/projectadministration/src/main/java/io/reflectoring/coderadar/core/projectadministration/domain/Project.java
+++ coderadar-core/projectadministration/src/main/java/io/reflectoring/coderadar/core/projectadministration/domain/Project.java
@@ -3,9 +3,11 @@ package io.reflectoring.coderadar.core.projectadministration.domain;
 import javax.persistence.*;
 import lombok.Data;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
 
 import java.net.URL;
 import java.util.Date;
+import java.util.List;
 
 /** A coderadar project that defines the source of files that are to be analyzed. */
 @NodeEntity
@@ -14,12 +16,15 @@ public class Project {
   private Long id;
   private String name;
   private String workdirName;
-
-  // Information about the VCS
   private URL vcsUrl;
   private String vcsUsername;
   private String vcsPassword;
   private boolean vcsOnline;
   private Date vcsStart;
   private Date vcsEnd;
+
+  // The graph starts from a project and goes only in one direction.
+  // https://en.wikipedia.org/wiki/Directed_acyclic_graph
+  @Relationship("HAS")
+  private List<Module> modules;
 }
