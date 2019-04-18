17804b5d82eb9cd82c886c5872adadfe53e6b980
Sebastian Gabriel Trzpiot <mail@trzpiot.de>
2019-04-18 11:27:40 +0200

    Convert the entity RefreshToken to Neo4j

--- coderadar-core/projectadministration/src/main/java/io/reflectoring/coderadar/core/projectadministration/domain/User.java
+++ coderadar-core/projectadministration/src/main/java/io/reflectoring/coderadar/core/projectadministration/domain/User.java
@@ -3,6 +3,9 @@ package io.reflectoring.coderadar.core.projectadministration.domain;
 import javax.persistence.*;
 import lombok.Data;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.List;
 
 /** a user of application, who has to login to access to functionality */
 @NodeEntity
@@ -11,4 +14,7 @@ public class User {
   private Long id;
   private String username;
   private String password;
+
+  @Relationship("HAS")
+  private List<RefreshToken> refreshTokens;
 }
