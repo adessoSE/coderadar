c55059f8471fd6f7a31877f4bcd33bc43a8e7ef7
Maxim Atanasov <atanasovmaksim1@gmail.com>
2019-04-19 15:01:52 +0200

    add ability to select commits

--- coderadar-app/src/app/view/project-dashboard/compare-branches/compare-branches.component.ts
+++ coderadar-app/src/app/view/project-dashboard/compare-branches/compare-branches.component.ts
@@ -1,8 +1,7 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {createGitgraph, Mode} from '@gitgraph/js';
+import {createGitgraph, Orientation} from '@gitgraph/js';
 import {Commit} from '../../../model/commit';
 import {AppEffects} from '../../../city-map/shared/effects';
-import { createCustomElement } from '@angular/elements';
 import {Project} from '../../../model/project';
 import {GitgraphUserApi} from '@gitgraph/core';
 
@@ -13,6 +12,11 @@ import {GitgraphUserApi} from '@gitgraph/core';
   styleUrls: ['./compare-branches.component.scss']
 })
 export class CompareBranchesComponent implements OnInit {
+
+  constructor(private cityEffects: AppEffects) {
+    this.selectedCommit1 = null;
+    this.selectedCommit2 = null;
+  }
 
   @ViewChild('graph', {read: ElementRef})graph: ElementRef;
   @Input() commits: Commit[];
@@ -26,38 +30,7 @@ export class CompareBranchesComponent implements OnInit {
 
   private gitgraph: GitgraphUserApi<SVGElement>;
 
-  constructor(private cityEffects: AppEffects) {
-    this.selectedCommit1 = null;
-    this.selectedCommit2 = null;
-  }
-
-  ngOnInit() {
-    this.commits.reverse();
-    this.gitgraph = createGitgraph(this.graph.nativeElement);
-    const master = this.gitgraph.branch('master');
-
-    this.commits.forEach(value => {
-      master.commit({
-        hash: value.name,
-        subject: new Date(value.timestamp).toDateString(),
-        author: value.author
-        });
-      });
-
-    const newFeature = this.gitgraph.branch('new-feature');
-    newFeature.commit('Implement an awesome feature');
-    newFeature.commit('Fix tests');
-
-    const newBranch = this.gitgraph.branch('fixes');
-    newBranch.commit('Implemented someting');
-    newBranch.commit('Fix something');
-
-    master.merge(newFeature, 'Release new version');
-    master.merge(newBranch, 'Release new version');
-
-  }
-
-  private extractCommitHash(markup: any): string {
+  private static extractCommitHash(markup: any): string {
     let result = '';
     if (markup.tagName === 'text') {
       for (let i = 0; markup.innerHTML.charAt(i) !== ' '; i++) {
@@ -73,12 +46,58 @@ export class CompareBranchesComponent implements OnInit {
     return null;
   }
 
+  ngOnInit() {
+    this.gitgraph = createGitgraph(this.graph.nativeElement, {orientation: Orientation.VerticalReverse});
+    this.redraw();
+  }
+
+  redraw() {
+
+    this.gitgraph.clear();
+    const master = this.gitgraph.branch('master');
+
+    this.commits.forEach(value => {
+      if (value === this.selectedCommit1 || value === this.selectedCommit2) {
+        master.commit({
+          hash: value.name,
+          subject: new Date(value.timestamp).toDateString(),
+          author: value.author,
+          style: {
+            dot: {
+              color: '#f60',
+            },
+            message: {
+              color: '#f60'
+            },
+          }
+        });
+      } else {
+        master.commit({
+          hash: value.name,
+          subject: new Date(value.timestamp).toDateString(),
+          author: value.author
+        });
+      }
+    });
+
+    const newFeature = this.gitgraph.branch('new-feature');
+    newFeature.commit('Implement an awesome feature');
+    newFeature.commit('Fix tests');
+
+    const newBranch = this.gitgraph.branch('fixes');
+    newBranch.commit('Implemented someting');
+    newBranch.commit('Fix something');
+
+    master.merge(newFeature, 'Release new version');
+    master.merge(newBranch, 'Release new version');
+  }
+
   public selectCommit(selectedCommitHash: EventTarget): void {
     if (!(selectedCommitHash instanceof SVGTextElement) &&
       !(selectedCommitHash instanceof SVGUseElement)) {
       return;
     }
-    const hash = this.extractCommitHash(selectedCommitHash);
+    const hash = CompareBranchesComponent.extractCommitHash(selectedCommitHash);
     if (hash !== null) {
       const selectedCommit = this.commits.find(value1 => value1.name.slice(0, 7) === hash);
       if (this.selectedCommit1 === null && this.selectedCommit2 !== selectedCommit) {
@@ -95,8 +114,7 @@ export class CompareBranchesComponent implements OnInit {
 
       this.cityEffects.firstCommit = this.selectedCommit1;
       this.cityEffects.secondCommit = this.selectedCommit2;
-      console.log(this.selectedCommit1);
-      console.log(this.selectedCommit2);
+      this.redraw();
     }
   }
 }
