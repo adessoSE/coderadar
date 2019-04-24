83d8fbeafabd7ffa285df0edc78cc165cc4a9df2
maximAtanasov <Maksim.Atanasov@adesso.de>
2019-04-24 19:10:32 +0200

    attemt to optimize commit selection

--- coderadar-app/src/app/view/project-dashboard/compare-branches/compare-branches.component.ts
+++ coderadar-app/src/app/view/project-dashboard/compare-branches/compare-branches.component.ts
@@ -1,12 +1,11 @@
-import {AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, Input, OnInit, ViewChild} from '@angular/core';
 import {createGitgraph, Orientation} from '@gitgraph/js';
 import {Commit} from '../../../model/commit';
 import {AppEffects} from '../../../city-map/shared/effects';
 import {Project} from '../../../model/project';
-import {GitgraphUserApi, BranchUserApi} from '@gitgraph/core';
-import {CdkScrollable, ScrollDispatcher} from '@angular/cdk/scrolling';
-import {MatSidenavContainer, ScrollDirection} from '@angular/material';
-import {Directionality} from '@angular/cdk/bidi';
+import {BranchUserApi, GitgraphUserApi} from '@gitgraph/core';
+import {el} from '@angular/platform-browser/testing/src/browser_util';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -14,10 +13,9 @@ import {Directionality} from '@angular/cdk/bidi';
   templateUrl: './compare-branches.component.html',
   styleUrls: ['./compare-branches.component.scss']
 })
-export class CompareBranchesComponent implements OnInit, AfterViewInit {
+export class CompareBranchesComponent implements OnInit {
 
-
-  constructor(private cityEffects: AppEffects,  private scrollDispatcher: ScrollDispatcher) {
+  constructor(private cityEffects: AppEffects) {
     this.selectedCommit1 = null;
     this.selectedCommit2 = null;
   }
@@ -28,15 +26,28 @@ export class CompareBranchesComponent implements OnInit, AfterViewInit {
   @Input() project: Project;
 
   public selectedCommit1: Commit;
+  private selectedCommit1Circle: SVGCircleElement;
+  private selectedCommit1Text: SVGTextElement;
+
   public selectedCommit2: Commit;
+  private selectedCommit2Circle: SVGCircleElement;
+  private selectedCommit2Text: SVGTextElement;
 
   public prevSelectedCommit1: Commit;
+  private prevSelectedCommit1Circle: SVGCircleElement;
+  private prevSelectedCommit1Text: SVGTextElement;
+
   public prevSelectedCommit2: Commit;
+  private prevSelectedCommit2Circle: SVGCircleElement;
+  private prevSelectedCommit2Text: SVGTextElement;
 
   private loadIndex = 15;
   private windowHeight = window.screen.availHeight;
 
   private gitgraph: GitgraphUserApi<SVGElement>;
+
+  private selectedCommitColor = '#f60';
+  private deselectedCommitColor = '#979797';
 
   master: BranchUserApi<SVGElement>;
 
@@ -56,32 +67,24 @@ export class CompareBranchesComponent implements OnInit, AfterViewInit {
     return null;
   }
 
-  ngAfterViewInit() {
-/*    this.scrollDispatcher.scrolled().subscribe(x => console.log(this.sidenavContainer.scrollable.measureScrollOffset('start')));
-    this.sidenavContainer.scrollable.elementScrolled().subscribe(x => {
-      console.log(x);
-    });*/
-  }
-
   ngOnInit() {
     this.gitgraph = createGitgraph(this.graph.nativeElement, {orientation: Orientation.VerticalReverse});
     this.redraw();
-
   }
 
   redraw() {
 
     this.gitgraph.clear();
-    this.master = this.gitgraph.branch('master');
+    this.master = this.gitgraph.branch( 'master');
 
     for (let i = 0; i < this.loadIndex && i < this.commits.length; i++) {
       if (i === 7) { // dummy commits
         const newFeature = this.gitgraph.branch('new-feature');
-        newFeature.commit('Implement an awesome feature');
+        newFeature.commit('Implemented an awesome feature');
         newFeature.commit('Fix tests');
 
         const newBranch = this.gitgraph.branch('fixes');
-        newBranch.commit('Implemented someting');
+        newBranch.commit('Implemented something');
         newBranch.commit('Fix something');
 
         this.master.merge(newFeature, 'Release new version');
@@ -95,10 +98,10 @@ export class CompareBranchesComponent implements OnInit, AfterViewInit {
           author: value.author,
           style: {
             dot: {
-              color: '#f60',
+              color: this.selectedCommitColor
             },
             message: {
-              color: '#f60'
+              color: this.selectedCommitColor
             },
           }
         });
@@ -110,33 +113,81 @@ export class CompareBranchesComponent implements OnInit, AfterViewInit {
         });
       }
     }
-
-
   }
 
-  public selectCommit(selectedCommitHash: EventTarget): void {
-    if (!(selectedCommitHash instanceof SVGTextElement) &&
-      !(selectedCommitHash instanceof SVGUseElement)) {
+  public handleClickEvent(selectedCommitHash: EventTarget): void {
+    let selectedCommitHashSVGCircle: SVGCircleElement;
+    let selectedCommitHashSVGText: SVGTextElement;
+
+    if (selectedCommitHash instanceof SVGUseElement) {
+      selectedCommitHashSVGCircle = selectedCommitHash.parentElement.getElementsByTagName('defs').item(0)
+        .getElementsByTagName('circle').item(0);
+      selectedCommitHashSVGText = selectedCommitHash.parentElement.parentElement.getElementsByTagName('g').item(1)
+        .getElementsByTagName('text').item(0);
+
+      selectedCommitHashSVGText.style.fill = this.selectedCommitColor;
+      selectedCommitHashSVGCircle.style.fill = this.selectedCommitColor;
+
+    } else if (selectedCommitHash instanceof SVGTextElement) {
+      selectedCommitHashSVGText = selectedCommitHash;
+      selectedCommitHashSVGCircle = selectedCommitHash.parentElement.parentElement.parentElement.getElementsByTagName('g').item(0)
+        .getElementsByTagName('defs').item(0).getElementsByTagName('circle').item(0);
+
+      selectedCommitHashSVGCircle.style.fill = this.selectedCommitColor;
+      selectedCommitHashSVGText.style.fill = this.selectedCommitColor;
+
+    } else {
       return;
     }
+
+
     const hash = CompareBranchesComponent.extractCommitHash(selectedCommitHash);
     if (hash !== null) {
       const selectedCommit = this.commits.find(value1 => value1.name.slice(0, 7) === hash);
+
       if (this.selectedCommit1 === null && this.selectedCommit2 !== selectedCommit) {
+        if (this.selectedCommit1Text !== undefined) {
+          this.selectedCommit1Circle.style.fill = this.deselectedCommitColor;
+          this.selectedCommit1Text.style.fill = this.deselectedCommitColor;
+        }
         this.selectedCommit1 = selectedCommit;
+        this.selectedCommit1Circle = selectedCommitHashSVGCircle;
+        this.selectedCommit1Text = selectedCommitHashSVGText;
+        this.selectedCommit1Circle.style.fill = this.selectedCommitColor;
+        this.selectedCommit1Text.style.fill = this.selectedCommitColor;
+
       } else if (this.selectedCommit1 === selectedCommit) {
         this.selectedCommit1 = null;
+        this.selectedCommit1Circle.style.fill = this.deselectedCommitColor;
+        this.selectedCommit1Text.style.fill = this.deselectedCommitColor;
+
         this.prevSelectedCommit1 = selectedCommit;
+        this.prevSelectedCommit1Circle = selectedCommitHashSVGCircle;
+        this.prevSelectedCommit1Text = selectedCommitHashSVGText;
+
       } else if (this.selectedCommit2 === selectedCommit) {
         this.selectedCommit2 = null;
+        this.selectedCommit2Circle.style.fill = this.deselectedCommitColor;
+        this.selectedCommit2Text.style.fill = this.deselectedCommitColor;
+
         this.prevSelectedCommit2 = selectedCommit;
+        this.prevSelectedCommit2Circle = selectedCommitHashSVGCircle;
+        this.prevSelectedCommit2Text = selectedCommitHashSVGText;
+
       } else {
+        if (this.selectedCommit2Text !== undefined) {
+          this.selectedCommit2Circle.style.fill = this.deselectedCommitColor;
+          this.selectedCommit2Text.style.fill = this.deselectedCommitColor;
+        }
         this.selectedCommit2 = selectedCommit;
+        this.selectedCommit2Circle = selectedCommitHashSVGCircle;
+        this.selectedCommit2Text = selectedCommitHashSVGText;
+        this.selectedCommit2Circle.style.fill = this.selectedCommitColor;
+        this.selectedCommit2Text.style.fill = this.selectedCommitColor;
       }
 
       this.cityEffects.firstCommit = this.selectedCommit1;
       this.cityEffects.secondCommit = this.selectedCommit2;
-      this.redraw();
     }
   }
 
@@ -145,32 +196,30 @@ export class CompareBranchesComponent implements OnInit, AfterViewInit {
     if (window.pageYOffset > (this.windowHeight + window.screenY - 700)) {
       for (let i = this.loadIndex; i < this.loadIndex + 15 && i < this.commits.length; i++) {
         const value = this.commits[i];
-        if (value === this.selectedCommit1 || value === this.selectedCommit2) {
-          this.master.commit({
-            hash: value.name,
-            subject: new Date(value.timestamp).toDateString(),
-            author: value.author,
-            style: {
-              dot: {
-                color: '#f60',
-              },
-              message: {
-                color: '#f60'
-              },
-            }
-          });
-        } else {
-          this.master.commit({
+        this.master.commit({
             hash: value.name,
             subject: new Date(value.timestamp).toDateString(),
             author: value.author
           });
-        }
       }
       this.loadIndex += 15;
       this.windowHeight = window.pageYOffset + window.screen.height * 1.5;
     }
+    const elementsText = document.getElementsByTagName('g').item(0).getElementsByTagName('g').item(1).getElementsByTagName('text');
+    const elementsCircle = document.getElementsByTagName('g').item(0).getElementsByTagName('g').item(1).getElementsByTagName('defs');
 
+    if (this.selectedCommit1 !== null) {
+      this.selectedCommit1Circle = elementsCircle.item(this.commits.indexOf(this.selectedCommit1)).getElementsByTagName('circle').item(0);
+      this.selectedCommit1Text =  elementsText.item(this.commits.indexOf(this.selectedCommit1));
+      this.selectedCommit1Circle.style.fill = this.selectedCommitColor;
+      this.selectedCommit1Text.style.fill = this.selectedCommitColor;
+    }
+    if (this.selectedCommit2 !== null) {
+      this.selectedCommit2Circle = elementsCircle.item(this.commits.indexOf(this.selectedCommit2)).getElementsByTagName('circle').item(0);
+      this.selectedCommit2Text =  elementsText.item(this.commits.indexOf(this.selectedCommit2));
+      this.selectedCommit2Circle.style.fill = this.selectedCommitColor;
+      this.selectedCommit2Text.style.fill = this.selectedCommitColor;
+    }
   }
 }
 
