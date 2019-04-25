1d1c2d01f7ccabaf097d89bc28885a03ec7a0afd
maximAtanasov <Maksim.Atanasov@adesso.de>
2019-04-25 08:37:35 +0200

    scrolling+selection mostly works

--- coderadar-app/src/app/view/project-dashboard/compare-branches/compare-branches.component.ts
+++ coderadar-app/src/app/view/project-dashboard/compare-branches/compare-branches.component.ts
@@ -33,14 +33,6 @@ export class CompareBranchesComponent implements OnInit {
   private selectedCommit2Circle: SVGCircleElement;
   private selectedCommit2Text: SVGTextElement;
 
-  public prevSelectedCommit1: Commit;
-  private prevSelectedCommit1Circle: SVGCircleElement;
-  private prevSelectedCommit1Text: SVGTextElement;
-
-  public prevSelectedCommit2: Commit;
-  private prevSelectedCommit2Circle: SVGCircleElement;
-  private prevSelectedCommit2Text: SVGTextElement;
-
   private loadIndex = 15;
   private windowHeight = window.screen.availHeight;
 
@@ -56,6 +48,9 @@ export class CompareBranchesComponent implements OnInit {
     if (markup.tagName === 'text') {
       for (let i = 0; markup.innerHTML.charAt(i) !== ' '; i++) {
         result += markup.innerHTML.charAt(i);
+        if (i > 7) {
+          return null;
+        }
       }
       return result;
     } else if (markup.tagName === 'use') {
@@ -161,18 +156,10 @@ export class CompareBranchesComponent implements OnInit {
         this.selectedCommit1Circle.style.fill = this.deselectedCommitColor;
         this.selectedCommit1Text.style.fill = this.deselectedCommitColor;
 
-        this.prevSelectedCommit1 = selectedCommit;
-        this.prevSelectedCommit1Circle = selectedCommitHashSVGCircle;
-        this.prevSelectedCommit1Text = selectedCommitHashSVGText;
-
       } else if (this.selectedCommit2 === selectedCommit) {
         this.selectedCommit2 = null;
         this.selectedCommit2Circle.style.fill = this.deselectedCommitColor;
         this.selectedCommit2Text.style.fill = this.deselectedCommitColor;
-
-        this.prevSelectedCommit2 = selectedCommit;
-        this.prevSelectedCommit2Circle = selectedCommitHashSVGCircle;
-        this.prevSelectedCommit2Text = selectedCommitHashSVGText;
 
       } else {
         if (this.selectedCommit2Text !== undefined) {
@@ -205,18 +192,35 @@ export class CompareBranchesComponent implements OnInit {
       this.loadIndex += 15;
       this.windowHeight = window.pageYOffset + window.screen.height * 1.5;
     }
+
     const elementsText = document.getElementsByTagName('g').item(0).getElementsByTagName('g').item(1).getElementsByTagName('text');
     const elementsCircle = document.getElementsByTagName('g').item(0).getElementsByTagName('g').item(1).getElementsByTagName('defs');
 
+
+    const textElementsFiltered = [];
+    for (let i = 0; i < elementsText.length; i++) {
+      if (CompareBranchesComponent.extractCommitHash(elementsText.item(i)) !== null) {
+        textElementsFiltered.push(elementsText.item(i));
+      }
+    }
+
     if (this.selectedCommit1 !== null) {
-      this.selectedCommit1Circle = elementsCircle.item(this.commits.indexOf(this.selectedCommit1)).getElementsByTagName('circle').item(0);
-      this.selectedCommit1Text =  elementsText.item(this.commits.indexOf(this.selectedCommit1));
+      let index = this.commits.indexOf(this.selectedCommit1);
+      if (index > 6) {
+        index += 6;
+      }
+      this.selectedCommit1Circle = elementsCircle.item(index).getElementsByTagName('circle').item(0);
+      this.selectedCommit1Text =  textElementsFiltered[index];
       this.selectedCommit1Circle.style.fill = this.selectedCommitColor;
       this.selectedCommit1Text.style.fill = this.selectedCommitColor;
     }
     if (this.selectedCommit2 !== null) {
-      this.selectedCommit2Circle = elementsCircle.item(this.commits.indexOf(this.selectedCommit2)).getElementsByTagName('circle').item(0);
-      this.selectedCommit2Text =  elementsText.item(this.commits.indexOf(this.selectedCommit2));
+      let index = this.commits.indexOf(this.selectedCommit2);
+      if (index > 6) {
+        index += 6;
+      }
+      this.selectedCommit2Circle = elementsCircle.item(index).getElementsByTagName('circle').item(0);
+      this.selectedCommit2Text =  textElementsFiltered[index];
       this.selectedCommit2Circle.style.fill = this.selectedCommitColor;
       this.selectedCommit2Text.style.fill = this.selectedCommitColor;
     }
