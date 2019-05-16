1423ec8192f1e6ebb472d1ddbc5b2d079199b37b
Sebastian Gabriel Trzpiot <mail@trzpiot.de>
2019-05-16 15:21:39 +0200

    Replace annotations

--- coderadar-core/analyzer/src/main/java/io/reflectoring/coderadar/core/analyzer/domain/Finding.java
+++ coderadar-core/analyzer/src/main/java/io/reflectoring/coderadar/core/analyzer/domain/Finding.java
@@ -1,27 +1,14 @@
 package io.reflectoring.coderadar.core.analyzer.domain;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import lombok.Data;
+import org.neo4j.ogm.annotation.NodeEntity;
 
-@Entity
-@Table(name = "finding")
+@NodeEntity
 @Data
 public class Finding {
-
-  @EmbeddedId private MetricValue id;
-
-  @Column(name = "line_start")
+  private Long id;
   private Integer lineStart;
-
-  @Column(name = "line_end")
   private Integer lineEnd;
-
-  @Column(name = "char_start")
   private Integer charStart;
-
-  @Column(name = "char_end")
   private Integer charEnd;
 }
--- coderadar-graph/src/main/java/io/reflectoring/coderadar/graph/analyzer/repository/GetAnalyzingStatusRepository.java
+++ coderadar-graph/src/main/java/io/reflectoring/coderadar/graph/analyzer/repository/GetAnalyzingStatusRepository.java
@@ -2,13 +2,11 @@ package io.reflectoring.coderadar.graph.analyzer.repository;
 
 import io.reflectoring.coderadar.core.projectadministration.domain.AnalyzingJob;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GetAnalyzingStatusRepository extends Neo4jRepository<AnalyzingJob, Long> {
-  @Query()
   // TODO: Add query to find a AnalyzingJob in spezific project.
   Optional<AnalyzingJob> findByProject_Id(Long projectId);
 }
