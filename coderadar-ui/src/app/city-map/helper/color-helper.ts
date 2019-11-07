7af08d7cd4360161a6e9bee9d417937f3204c8d6
Lorenz Voßgätter <Lorenz.Vossgaetter@adesso.de>
2019-11-07 13:23:10 +0100

    more consistent color types

--- coderadar-ui/src/app/city-map/helper/color-helper.ts
+++ coderadar-ui/src/app/city-map/helper/color-helper.ts
@@ -6,12 +6,20 @@ declare var chroma: any;
 
 export class ColorHelper {
 
-  static getColorByPosition(screenType: ScreenType): string {
-    return screenType === ScreenType.LEFT ? VisualizationConfig.COLOR_FIRST_COMMIT : VisualizationConfig.COLOR_SECOND_COMMIT;
+  static getColorByPosition(screenType: ScreenType): Color {
+    return new Color(screenType === ScreenType.LEFT ? VisualizationConfig.COLOR_FIRST_COMMIT : VisualizationConfig.COLOR_SECOND_COMMIT);
   }
 
-  static getContraryColorByColor(color: string): string {
-    return color === VisualizationConfig.COLOR_FIRST_COMMIT ? VisualizationConfig.COLOR_SECOND_COMMIT : VisualizationConfig.COLOR_FIRST_COMMIT;
+  static colorFromHex(color:string):Color{
+    return new Color(color);
+  }
+
+  static hexFromColor(color:Color):string{
+    return color.getHexString();
+  }
+
+  static getContraryColorByColor(color: Color): Color {
+    return ColorHelper.colorFromHex(ColorHelper.hexFromColor(color) === VisualizationConfig.COLOR_FIRST_COMMIT ? VisualizationConfig.COLOR_SECOND_COMMIT : VisualizationConfig.COLOR_FIRST_COMMIT);
   }
 
   static getColorByMetricValue(value: number, max: number, min: number): Color {
