1e247330a1ead499708a6390a4da3b408cb7e819
Krause <Kilian.Krause@adesso.de>
2020-03-13 12:25:17 +0100

    add integration tests and rest api docs

--- coderadar-test/src/test/java/io/reflectoring/coderadar/rest/contributor/UpdateContributorControllerIntegrationTest.java
+++ coderadar-test/src/test/java/io/reflectoring/coderadar/rest/contributor/UpdateContributorControllerIntegrationTest.java
@@ -1,18 +1,24 @@
 package io.reflectoring.coderadar.rest.contributor;
 
-import static io.reflectoring.coderadar.rest.JsonHelper.fromJson;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import io.reflectoring.coderadar.contributor.port.driver.UpdateContributorCommand;
+import io.reflectoring.coderadar.graph.contributor.domain.ContributorEntity;
+import io.reflectoring.coderadar.graph.contributor.repository.ContributorRepository;
 import io.reflectoring.coderadar.projectadministration.port.driver.project.create.CreateProjectCommand;
 import io.reflectoring.coderadar.rest.ControllerTestTemplate;
-import io.reflectoring.coderadar.rest.domain.IdResponse;
 import java.net.URL;
 import java.util.Objects;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
 
 public class UpdateContributorControllerIntegrationTest extends ControllerTestTemplate {
+  @Autowired private ContributorRepository contributorRepository;
 
   @Test
   public void updateContributor() throws Exception {
@@ -26,12 +32,36 @@ public class UpdateContributorControllerIntegrationTest extends ControllerTestTe
             false,
             null,
             null);
-    MvcResult result =
-        mvc()
-            .perform(
-                post("/projects").contentType(MediaType.APPLICATION_JSON).content(toJson(command1)))
-            .andReturn();
 
-    long projectId = fromJson(result.getResponse().getContentAsString(), IdResponse.class).getId();
+    mvc()
+        .perform(
+            post("/projects").contentType(MediaType.APPLICATION_JSON).content(toJson(command1)))
+        .andExpect(status().isCreated())
+        .andReturn();
+
+    long contributorId = contributorRepository.findAll().get(0).getId();
+    UpdateContributorCommand command = new UpdateContributorCommand("New DisplayName");
+    mvc()
+        .perform(
+            post("/contributors/" + contributorId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(command)))
+        .andExpect(status().isOk())
+        .andDo(documentUpdateContributor())
+        .andReturn();
+
+    ContributorEntity contributor = contributorRepository.findById(contributorId).get();
+
+    Assertions.assertThat(contributor.getDisplayName()).isEqualTo("New DisplayName");
+  }
+
+  private ResultHandler documentUpdateContributor() {
+    ConstrainedFields<UpdateContributorCommand> fields = fields(UpdateContributorCommand.class);
+    return document(
+        "contributors/update",
+        requestFields(
+            fields
+                .withPath("displayName")
+                .description("The new display name of the contributor.")));
   }
 }
