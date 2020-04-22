0858ea5650265b57187553e4a9066fe923cf1cd9
Krause <Kilian.Krause@adesso.de>
2020-04-22 16:20:43 +0200

    fix test

--- coderadar-test/src/test/java/io/reflectoring/coderadar/rest/query/GetFileDiffControllerTest.java
+++ coderadar-test/src/test/java/io/reflectoring/coderadar/rest/query/GetFileDiffControllerTest.java
@@ -35,7 +35,6 @@ public class GetFileDiffControllerTest extends ControllerTestTemplate {
             "password",
             Objects.requireNonNull(testRepoURL).toString(),
             false,
-            null,
             null);
     MvcResult result =
         mvc()
