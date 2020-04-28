d752bb8ad2b1f4e49ec3a34790d67a70c3024f18
Lorenz Voßgätter <lorenz.vossgaetter@web.de>
2020-04-28 13:21:43 +0200

    move dialogues into components folder

--- coderadar-ui/src/app/app.module.ts
+++ coderadar-ui/src/app/app.module.ts
@@ -9,10 +9,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AddProjectComponent} from './view/add-project/add-project.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {MainDashboardComponent, ConfirmDeleteProjectDialogComponent} from './view/main-dashboard/main-dashboard.component';
+import {MainDashboardComponent} from './view/main-dashboard/main-dashboard.component';
 import {LayoutModule} from '@angular/cdk/layout';
 import {AuthInterceptor} from './auth.interceptor';
-import {ConfigureProjectComponent, MergeDialogComponent} from './view/configure-project/configure-project.component';
+import {ConfigureProjectComponent} from './view/configure-project/configure-project.component';
 import {EditProjectComponent} from './view/edit-project/edit-project.component';
 import {HeaderComponent} from './view/header/header.component';
 import {FooterComponent} from './view/footer/footer.component';
@@ -64,6 +64,10 @@ import { ListViewComponent } from './view/project-dashboard/list-view/list-view.
 import { BranchViewComponent } from './view/project-dashboard/branch-view/branch-view.component';
 import {ContributorCardComponent, ContributorDialogComponent} from './components/contributor-card/contributor-card.component';
 import { FileViewComponent } from './view/file-view/file-view.component';
+import {ContributorMergeDialogComponent} from './components/contributor-merge-dialog/merge-dialog.component';
+import {
+  DeleteProjectDialogComponent
+} from './components/delete-project-dialog/delete-project-dialog.component';
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -89,7 +93,7 @@ const appRoutes: Routes = [
     RegisterComponent,
     AddProjectComponent,
     MainDashboardComponent,
-    ConfirmDeleteProjectDialogComponent,
+    DeleteProjectDialogComponent,
     ConfigureProjectComponent,
     EditProjectComponent,
     HeaderComponent,
@@ -99,7 +103,7 @@ const appRoutes: Routes = [
     ViewCommitComponent,
     CityViewComponent,
     DependencyRootComponent,
-    MergeDialogComponent,
+    ContributorMergeDialogComponent,
     DependencyCompareComponent,
     TreeNodeComponent,
     CityViewHeaderComponent,
@@ -107,7 +111,8 @@ const appRoutes: Routes = [
     BranchViewComponent,
     ContributorDialogComponent,
     ContributorCardComponent,
-    FileViewComponent
+    FileViewComponent,
+    DeleteProjectDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -175,9 +180,9 @@ const appRoutes: Routes = [
   ],
   bootstrap: [AppComponent],
   entryComponents: [
-    MergeDialogComponent,
+    ContributorMergeDialogComponent,
     ContributorDialogComponent,
-    ConfirmDeleteProjectDialogComponent
+    DeleteProjectDialogComponent
   ]
 })
 export class AppModule {
