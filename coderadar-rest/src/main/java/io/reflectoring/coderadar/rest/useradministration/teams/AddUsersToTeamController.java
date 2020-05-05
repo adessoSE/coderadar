2bd9cf584d081205e3af149b62c9424e1e9f175f
Krause <Kilian.Krause@adesso.de>
2020-05-05 16:14:24 +0200

    implement AddUsersToTeamController

--- coderadar-rest/src/main/java/io/reflectoring/coderadar/rest/useradministration/teams/AddUsersToTeamController.java
+++ coderadar-rest/src/main/java/io/reflectoring/coderadar/rest/useradministration/teams/AddUsersToTeamController.java
@@ -2,8 +2,16 @@ package io.reflectoring.coderadar.rest.useradministration.teams;
 
 import io.reflectoring.coderadar.rest.AbstractBaseController;
 import io.reflectoring.coderadar.useradministration.port.driver.teams.AddUsersToTeamUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Transactional
@@ -12,5 +20,11 @@ public class AddUsersToTeamController implements AbstractBaseController {
 
     public AddUsersToTeamController(AddUsersToTeamUseCase addUsersToTeamUseCase) {
         this.addUsersToTeamUseCase = addUsersToTeamUseCase;
+    }
+
+    @PostMapping(path = "/teams/{teamId}/users", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<HttpStatus> addUsersToTeam(@PathVariable long teamId, @RequestBody List<Long> userIds) {
+        addUsersToTeamUseCase.addUsersToTeam(teamId, userIds);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
