facdda232d9cf3f1e48fa94a0d0d0ede7fa9bc85
Kilian Krause <KilianKrause@outlook.de>
2020-06-03 15:51:51 +0200

    make sure that controllers consume and produce JSON values where necessary

--- coderadar-rest/src/main/java/io/reflectoring/coderadar/rest/GetModuleResponseMapper.java
+++ coderadar-rest/src/main/java/io/reflectoring/coderadar/rest/GetModuleResponseMapper.java
@@ -2,22 +2,21 @@ package io.reflectoring.coderadar.rest;
 
 import io.reflectoring.coderadar.projectadministration.domain.Module;
 import io.reflectoring.coderadar.rest.domain.GetModuleResponse;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GetModuleResponseMapper {
-    private GetModuleResponseMapper() {}
+  private GetModuleResponseMapper() {}
 
-    public static GetModuleResponse mapModule(Module module) {
-        return new GetModuleResponse(module.getId(), module.getPath());
-    }
+  public static GetModuleResponse mapModule(Module module) {
+    return new GetModuleResponse(module.getId(), module.getPath());
+  }
 
-    public static List<GetModuleResponse> mapModules(List<Module> modules) {
-        List<GetModuleResponse> responses = new ArrayList<>(modules.size());
-        for (Module module : modules) {
-            responses.add(mapModule(module));
-        }
-        return responses;
+  public static List<GetModuleResponse> mapModules(List<Module> modules) {
+    List<GetModuleResponse> responses = new ArrayList<>(modules.size());
+    for (Module module : modules) {
+      responses.add(mapModule(module));
     }
+    return responses;
+  }
 }
--- coderadar-rest/src/main/java/io/reflectoring/coderadar/rest/filepattern/GetFilePatternController.java
+++ coderadar-rest/src/main/java/io/reflectoring/coderadar/rest/filepattern/GetFilePatternController.java
@@ -1,5 +1,7 @@
 package io.reflectoring.coderadar.rest.filepattern;
 
+import static io.reflectoring.coderadar.rest.GetFilePatternResponseMapper.mapFilePattern;
+
 import io.reflectoring.coderadar.projectadministration.domain.FilePattern;
 import io.reflectoring.coderadar.projectadministration.port.driver.filepattern.get.GetFilePatternUseCase;
 import io.reflectoring.coderadar.rest.AbstractBaseController;
@@ -11,8 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import static io.reflectoring.coderadar.rest.GetFilePatternResponseMapper.mapFilePattern;
 
 @Transactional
 @RestController
@@ -29,7 +29,6 @@ public class GetFilePatternController implements AbstractBaseController {
   public ResponseEntity<GetFilePatternResponse> getFilePattern(
       @PathVariable(name = "filePatternId") long filePatternId) {
     FilePattern filePattern = getFilePatternUseCase.get(filePatternId);
-    return new ResponseEntity<>(mapFilePattern(filePattern),
-        HttpStatus.OK);
+    return new ResponseEntity<>(mapFilePattern(filePattern), HttpStatus.OK);
   }
 }
